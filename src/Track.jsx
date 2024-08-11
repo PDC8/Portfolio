3cbcbdffd85c5f66028969fa9602a243a4f11ab5
PDongC <peidongchen2004@gmail.com>
2024-08-11 15:49:37 -0400

    testing glb objects

--- src/Track.jsx
+++ src/Track.jsx
@@ -1,25 +1,25 @@
-import { useRef, useEffect } from 'react'
-import { useLoader } from '@react-three/fiber';
-import { usePlane } from '@react-three/cannon';
-import { useGLTF, MeshReflectorMaterial } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/Addons.js';
-import trackObj from './Assets/track.glb';
-import { ColliderBox } from './ColliderBox';
+// import { useRef, useEffect } from 'react'
+// import { useLoader } from '@react-three/fiber';
+// import { usePlane } from '@react-three/cannon';
+// import { useGLTF, MeshReflectorMaterial } from '@react-three/drei';
+// import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+// import trackObj from './Assets/track.glb';
+// import { ColliderBox } from './ColliderBox';
 
-export function Track(){
-    const result = useLoader(
-        GLTFLoader,
-        trackObj
-    );
+// export function Track(){
+//     const result = useLoader(
+//         GLTFLoader,
+//         trackObj
+//     );
 
-    let geometry = result.scene.children[0].geometry;
+//     let geometry = result.scene.children[0].geometry;
 
-    return (
-        <>
-            <mesh>
-                <primitive object={geometry} attach={'geometry'} />
-            </mesh>
-            <ColliderBox position={[10, 0, 10]} scale={[1, 1, 1]} />
-        </> 
-    )
-}
+//     return (
+//         <>
+//             <mesh>
+//                 <primitive object={geometry} attach={'geometry'} />
+//             </mesh>
+//             <ColliderBox position={[10, 0, 10]} scale={[1, 1, 1]} />
+//         </> 
+//     )
+// }
